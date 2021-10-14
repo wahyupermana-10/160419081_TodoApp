9bd5af8f6fe4411224474b9b0227ff4fe2195899
Risqi <wahyupermana51@gmail.com>
2021-10-14 14:50:59 +0900

    init

---
+++ app/src/main/java/com/ubaya/a160419081_todoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.a160419081_todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
