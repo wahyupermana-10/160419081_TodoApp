735e8d09fdd535a8d3bf3f9ac730e65fe47a4711
Risqi <wahyupermana51@gmail.com>
2021-10-21 14:35:09 +0900

    room week 9 fix 4

--- app/src/main/java/com/ubaya/a160419081_todoapp/model/TodoDatabase.kt
+++ app/src/main/java/com/ubaya/a160419081_todoapp/model/TodoDatabase.kt
@@ -4,8 +4,9 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.ubaya.a160419081_todoapp.util.MIGRATION_1_2
 
-@Database(entities = arrayOf(Todo::class), version = 1)
+@Database(entities = arrayOf(Todo::class), version = 2)
 abstract class TodoDatabase:RoomDatabase() {
     abstract fun todoDao(): TodoDao
 
@@ -16,10 +17,10 @@ abstract class TodoDatabase:RoomDatabase() {
         private fun buildDatabase(context: Context) = Room.databaseBuilder(
             context.applicationContext,
             TodoDatabase::class.java,
-        "tododb"
-        ).build()
-
-
+            "tododb"
+        )
+            .addMigrations(MIGRATION_1_2)
+            .build()
 
         operator fun invoke(context: Context)= instance ?: synchronized(LOCK) {
             instance ?: buildDatabase(context).also {
--- app/src/main/java/com/ubaya/a160419081_todoapp/viewmodel/DetailTodoViewModel.kt
+++ app/src/main/java/com/ubaya/a160419081_todoapp/viewmodel/DetailTodoViewModel.kt
@@ -2,22 +2,40 @@ package com.ubaya.a160419081_todoapp.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.room.Room
 import com.ubaya.a160419081_todoapp.model.Todo
 import com.ubaya.a160419081_todoapp.model.TodoDatabase
+import com.ubaya.a160419081_todoapp.util.buildDB
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import java.util.*
 import kotlin.coroutines.CoroutineContext
 
 class DetailTodoViewModel(application: Application):AndroidViewModel(application), CoroutineScope {
     private val job = Job()
+    val todoLD = MutableLiveData<Todo>()
+
+    fun fetch(uuid: Int){
+        launch {
+            val db = buildDB(getApplication())
+            todoLD.value = db.todoDao().selectTodo(uuid)
+        }
+    }
 
     fun addTodo(todo:Todo){
         launch {
-            val db = Room.databaseBuilder(getApplication(), TodoDatabase::class.java, "tododb").build()
+            val db = buildDB(getApplication())
             db.todoDao().insertAll(todo)
+        }
+    }
+
+    fun update(title:String, notes:String, priority:Int, uuid: Int){
+        launch {
+            val db = buildDB(getApplication())
+            db.todoDao().update(title, notes, priority, uuid)
         }
     }
     override val coroutineContext: CoroutineContext
