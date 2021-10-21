d7b9f13c0e24a53688d8e2a282d8b98b9318f536
Risqi <wahyupermana51@gmail.com>
2021-10-21 14:04:12 +0900

    room week 9 fix

--- app/src/main/java/com/ubaya/a160419081_todoapp/model/TodoDatabase.kt
+++ app/src/main/java/com/ubaya/a160419081_todoapp/model/TodoDatabase.kt
@@ -19,6 +19,7 @@ abstract class TodoDatabase:RoomDatabase() {
         "tododb"
         ).build()
 
+
         operator fun invoke(context: Context)= instance ?: synchronized(LOCK) {
             instance ?: buildDatabase(context).also {
                 instance = it
